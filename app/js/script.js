c8b65d79b2373c4bc690da6141df62b09c358472
Sarah Waldie <sareliz126@gmail.com>
2016-10-17 18:03:26 -0400

    angularized featured product gallery

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,31 @@
+function ListController( $scope ) {
+
+	$scope.entries = [
+	{
+		"title": "Blue Floral Scarf - $19.99",
+		"name": "Blue Floral",
+		"picture": ["bluefloral.jpg"],
+		"buylink": ""
+	},
+	{
+		"title": "Pink Squares Scarf - $19.99",
+		"name": "Pink Squares",
+		"picture": ["pinksquares.jpg"],
+		"buylink": ""
+	},
+	{
+		"title": "Blue Plaid Scarf - $19.99",
+		"name": "Blue Plaid",
+		"picture": ["plaid.jpg"],
+		"buylink": ""
+	},
+	]
+};
+
 var gallery = ["/img/newmusic.svg", "/img/greatgifts.svg", "/img/image3.svg"];
 var sliderPic = document.getElementById("sliderimage");
 
-	function changePicOne() {
+function changePicOne() {
 		// sliderPic.src = gallery[1] WHY DOESN'T THIS WORK?
 		document.getElementById("sliderimage").src = gallery[0]
 	}
@@ -14,26 +38,26 @@ var sliderPic = document.getElementById("sliderimage");
 		document.getElementById("sliderimage").src = gallery[2]
 	}
 
-window.onload = function () {
-	prepareGallery();
-}
+	window.onload = function () {
+		prepareGallery();
+	}
 
-function prepareGallery() {
-	var gallery = document.getElementById("imageGallery");
-	var links = gallery.getElementsByTagName("a");
+	function prepareGallery() {
+		var gallery = document.getElementById("imageGallery");
+		var links = gallery.getElementsByTagName("a");
 
-	showPicOne(links[0]);
+		showPicOne(links[0]);
 
-	for (var i=0; i < links.length; i++ ) {
-		links[i].onclick = function() {
-			showPicOne(this, links);
-			return false;
+		for (var i=0; i < links.length; i++ ) {
+			links[i].onclick = function() {
+				showPicOne(this, links);
+				return false;
+			}
 		}
 	}
-}
 
 // function showPicOne(whichPic, links) {
-function showPicOne(whichPic) {
+	function showPicOne(whichPic) {
 
 	// if(links){
 
@@ -55,5 +79,5 @@ function showPicOne(whichPic) {
 	
 	description.firstChild.nodeValue = text;
 
-	}
+}
 
