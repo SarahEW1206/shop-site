7adb5b19c612f0db0f17833f3f406ec40494c04a
Sarah Waldie <sareliz126@gmail.com>
2016-10-19 10:34:12 -0400

    fixed broken angular

--- app/js/script.js
+++ app/js/script.js
@@ -69,3 +69,22 @@ function showPicOne(whichPic, links) {
 
 }
 
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" showing", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " showing";
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
