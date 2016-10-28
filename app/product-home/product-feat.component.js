d8d85756431ec39759b9c5fdd98f2095ef026bba
Sarah Waldie <sareliz126@gmail.com>
2016-10-28 18:59:41 -0400

    hot mess

--- app/product-home/product-feat.component.js
+++ app/product-home/product-feat.component.js
@@ -1,15 +1,20 @@
 angular.module('productApp').component('productFeat', {
-    templateUrl: "product-home/product-feat-template.html",
+    templateUrl: "product-home/product-feat.template.html",
     controller: ['$http',
         function ProductFeatController($http) {
             var self = this;
 
+            self.setImage = function setImage(imageSrc) {
+                self.bigPic = imageSrc;
+            }
 
             $http.get('data/featured.json').then(function (response) {
                 self.featured = response.data;
+                self.setImage("img/bluefloral.jpg");
             });
 
-        }
 
+        }
     ]
 });
+
